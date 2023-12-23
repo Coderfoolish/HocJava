0da3738336f39156ca15435c2f02fd67e6eb15ec
Dino? <daophuduytan2203@gmail.com>
2023-12-23 22:58:59 +0700

    chua de 2022-2023

--- LTHDT/src/dethi2022_2023_clc/Author.java
+++ LTHDT/src/dethi2022_2023_clc/Author.java
@@ -1,2 +1,20 @@
-package dethi2022_2023_clc;public class Author {
+package dethi2022_2023_clc;
+
+public class Author {
+    private String name ;
+    private String email ;
+    private Character gender ;
+
+    public Author(){
+        name = null ;
+        email = null ;
+        gender = null ;
+    }
+    public Author(String name , String emai , Character gender)
+    {
+        this.name = name ;
+        this.email = emai ;
+        this.gender = gender ;
+    }
+
 }
--- LTHDT/src/dethi2022_2023_clc/Book.java
+++ LTHDT/src/dethi2022_2023_clc/Book.java
@@ -1,2 +1,93 @@
-package dethi2022_2023_clc;public class Book {
+package dethi2022_2023_clc;
+
+import java.util.Arrays;
+
+public class Book {
+    private String name ;
+    Author[] authors ;
+    private  double price ;
+    private int qty = 0 ;
+
+    public String getName() {
+        return name;
+    }
+    public Book()
+    {
+        name = null ;
+        Author[] authors = null ;
+        price = 0.0 ;
+    }
+//    public Book(String name , Author[] authors , double price )
+//    {
+//        this.name = name ;
+//        this.authors = authors ;
+//        this.price = price;
+//    }
+    public Book (String name , Author[] authors , double price , int qty)
+    {
+        this.name = name ;
+        this.authors = authors ;
+        this.price = price;
+        this.qty = qty ;
+    }
+    public Author[] getAuthors(){
+        return authors ;
+    }
+    public double getPrice(){
+        return price ;
+    }
+    public void setPrice(double price) {
+        this.price = price ;
+    }
+    public int getQty() {
+        return qty ;
+    }
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", authors=" + Arrays.toString(authors) +
+                ", price=" + price +
+                ", qty=" + qty +
+                '}';
+    }
+
+    public String getAuthorNames()
+    {
+        String[] temp = {"authorName1" , "authorName2"};
+        for (String s : temp){
+            System.out.println(s);
+        }
+        return null ;
+    }
+    public Book dauSachLonNhat(Book[] list)
+    {
+        if (list.length == 0 ) {
+            return  null ;
+        }
+        Book max = list[0] ;
+        for (Book q: list){
+            if(max.getQty() < q.getQty())
+                max = q;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+       Book[] oop = new Book[5];
+       Author[] oop1 = new Author[1];
+       oop1 = new Author[]{new Author("a", "abc", '1')};
+       oop[0] = new Book("A" , oop1 , 2.1 , 133515);
+       oop[1] = new Book("B" , oop1 , 2.1 , 123);
+       oop[2] = new Book("C" , oop1 , 2.1 , 13515);
+       oop[3] = new Book("C" , oop1 , 2.1 , 13515);
+       oop[4] = new Book("C" , oop1 , 2.1 , 13515);
+       Book abc = new Book() ;
+       abc = abc.dauSachLonNhat(oop) ;
+        System.out.println(abc);
+    }
 }
